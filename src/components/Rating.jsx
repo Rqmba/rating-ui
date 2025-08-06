4ad0839ee13a44bc18ed66db794ffb10860552ac
Rqmba <ramblack45@gmail.com>
2025-08-06 14:57:10 +0200

    Add rating state

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
-        {stars.map((star, index) => (
-          <span key={star} className="star">
+        {stars.map((star) => (
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className="star"
+          >
             {"\u2605"}
           </span>
         ))}
